238501bc2b612eaebc6022aa12000a084e968dc4
Fatma <Fatma@DESKTOP-SAP7Q3K>
2026-03-10 14:57:17 +0300

    Add project files.

---
+++ Sube1.EmlakciApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using Sube1.EmlakciLib;
+
+namespace Sube1.EmlakciApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ev evim = new Ev(3, 2, "Gazi", 100);
+            Console.WriteLine(evim.EvBilgileri());                       
+
+            var evim4 = new Ev(3, 5, 120);
+
+            string bilgi = evim4.EvBilgileri();
+            Console.WriteLine(bilgi);
+            //Console.WriteLine(evim4.EvBilgileri());            
+           
+        }
+    }
+
+
+
+  
+}
+
+//null: değersiz
+//Class'lar referans tipi veri tipleridir.Referans tipleri veriler, HEAP bölgesinde tutulur ve null değer alabilirler. Null referanslar üstünden işlem yapılırsa, Runtime sırasında NullReferenceException hatası alınır.
+//Constructor-Yapıcı Metod-Kurucu Metod: Class içinde tanımlanan field'lara varsayılan değer atamasını yapan metodlardır. Bu metodlar class içinde her zaman bulunurlar. Bir classtan nesne türetilir türetilmez çalışan ilk metoddur.
---
+++ Sube1.EmlakciLib/Ev.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Sube1.EmlakciLib
+{
+    public class Ev
+    {
+        public Ev()
+        {
+
+        }
+        public Ev(int odasayisi, int katno, string semt, double alan)
+        {
+            this.odasayisi = odasayisi;
+            this.katno = katno;
+            this.semt = semt;
+            this.alan = alan;
+        }
+        public Ev(int odasayisi, int katno, double alan)
+        {
+            this.alan = alan;
+            this.odasayisi = odasayisi;
+            this.katno = katno;
+            this.semt = "Gazi";
+        }
+
+        public int odasayisi;
+        public int katno;
+        public string semt;
+        public double alan;
+
+        public string EvBilgileri()
+        {
+            return $"Oda Sayısı:{this.odasayisi}\nKat No:{this.katno}\nSemt:{this.semt}\nAlan:{this.alan}m2";
+        }
+    }
+}
